b9cc8b57c4659b19a0e2c9f199776b1f8e74d5c5
Aditya Tayde <54738576+AdityaTayde13@users.noreply.github.com>
2021-06-29 10:55:32 -0700

    notesfuntion added with some tweaks

---
+++ NotesDispensed.py
@@ -0,0 +1,28 @@
+from tts import notestts,cantBdispense,aur, ultimate
+def nota(i):
+  count = 0
+  total = 0
+  splitup = {}
+  if i >= 2000:
+    splitup[2000] = int(i//2000)
+    i = i - splitup[2000]*2000
+  if i >= 500:
+    splitup[500] = int(i//500)
+    i = i - splitup[500]*500
+  if i >= 200:
+    splitup[200] = int(i//200)
+    i = i - splitup[200]*200
+  if i % 100 == 0 and i != 0:
+    splitup[100] = int(i/100)
+  else:
+    cantBdispense()
+    return None
+  for key,value in splitup.items():
+    count += 1
+    total += value
+    if count > 1 and count <= len(splitup):
+      aur()
+    notestts(key,value)
+
+  ultimate(total)
+    
