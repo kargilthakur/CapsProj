8f102d782d092904f4ad13380455d482658424c7
kargilthakur <49880502+kargilthakur@users.noreply.github.com>
2021-06-30 16:21:38 +0530

    Update Distancedemo.py

--- Distancedemo.py
+++ Distancedemo.py
@@ -8,23 +8,27 @@ import math as m
 os.remove('output.avi')
 cv2.startWindowThread()
 url = 'http://192.168.0.4:8080/video'
-
+# To capture the video
 cap = cv2.VideoCapture(url)
+# Initializing HOG descriptor
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+# For saving the output
 out = cv2.VideoWriter(
     'output.avi',
     cv2.VideoWriter_fourcc(*'MJPG'),
     15.,
     (640,480))
 
+# If the connection is established
 while(cap.isOpened()):    
     ret, frame = cap.read()
     if ret == False:
         break
     #cv2.imwrite('demo.jpg',frame)
     #image = cv2.imread("demo.jpg")
+#     Resize and recolor the frame
     frame = cv2.resize(frame, (640, 480))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
@@ -40,7 +44,7 @@ while(cap.isOpened()):
             l2.append((xA+xB)/2)
             l2.append((yA+yB)/2)
             l1.append(l2)
-            
+#        If two people are detected then find distance
     if len(boxes) == 2:
          a=l1[0]
          b=l1[1]
